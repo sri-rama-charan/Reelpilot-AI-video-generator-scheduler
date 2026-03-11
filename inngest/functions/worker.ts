8d16f9b0c3d8637824db65445cace6776941c663
sri-rama-charan <sriramcharan9999@gmail.com>
2026-03-11 12:50:58 +0530

    chore: register helloWorld on cloud run worker

--- inngest/functions/worker.ts
+++ inngest/functions/worker.ts
@@ -1,9 +1,11 @@
+import { helloWorld } from "@/inngest/functions/helloWorld";
 import { generateVideo } from "@/inngest/functions/generateVideo";
 import { publishWorker } from "@/inngest/functions/publishWorker";
 import { scheduleDaily } from "@/inngest/functions/scheduleDaily";
 import { youtubePublish } from "@/inngest/functions/youtubePublish";
 
 export const workerFunctions = [
+  helloWorld,
   generateVideo,
   publishWorker,
   scheduleDaily,
